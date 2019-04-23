477e1bc26a87da53cba2d8d42b0552a54215ed13
taka197 <taka.nomad168@gmail.com>
2019-04-23 20:31:52 +0900

    add new file

---
+++ k1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet"  href="style.css">
+</head>
+<center>
+<body>
+<div id="Member_registration"
+<form method="POST" action="k2.php">
+<h1>Member registration</h1>
+<p>Name</p><input type="text" name="name" />
+<p>Mail</p><input type="text" name="mail" />
+
+<p>Password</p><input type="text" name="pass" />
+<br><input type="submit" value="submit" />
+</form>
+</body>
+</div>
+</center>
+</html>
+
