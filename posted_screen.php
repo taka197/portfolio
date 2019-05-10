d31be6da8eef528f8df29ba0dac36504bbddd423
taka197 <49279903+taka197@users.noreply.github.com>
2019-05-10 12:46:55 +0900

    Update posted_screen.php

--- posted_screen.php
+++ posted_screen.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>掲示板</title>
+<title>Posts</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <center>
@@ -11,8 +11,8 @@
 <h1>Posted bulletin board</h1>
 	<li><a class="#active" href="#home">Home</a></li>
 	<li><a href="new_posts.php">New posts</a></li>
-	<li><a href="#contact">Posted lists</a></li>
-<li><a href="#contact">Contact</a></li>
+	<li><a href="#posted_lists">Posted lists</a></li>
+    <li><a href="#contact">Contact</a></li>
 <li>
 <?php
 session_start();
@@ -27,9 +27,14 @@ if (isset($_SESSION['name'])) {
 ?>
 </li>
 </ul>
+<?php
+if (isset($_SESSION['id'])) {
+	echo '<p class="welcome">Welcome</p>';
+}
+?>
 <div id="title">
-<br><br><a href="new_posts.php"> 新規投稿</a></br></br>
-<hr>
+<hr class="hr1">
+<div id="posted_lists">
 <h2>Posted Lists</h2>
 </hr>
 <?php
@@ -38,9 +43,9 @@ $user = 'hasegawa';
 $password = 'wY8MeVfuJy';
 try {
 	$dbh = new PDO($dsn, $user, $password);
-	$sql = "SELECT *FROM posted_screen";
-	$res = $dbh->query($sql);
-	$count = $res->rowCount();
+	$sql1 = "SELECT * FROM posted_screen";
+	$res1 = $dbh->query($sql1);
+	$count = $res1->rowCount();
 ?>
 </div>
 <table border="1">
@@ -48,29 +53,35 @@ try {
 <th>Posted ID</th>
 <th>Name</th>
 <th>Title</th>
-<?php
-	if(isset($_SESSION['name'])) {
-		echo '<a href="posts_update.php">編集</a>';
-		echo '<br><a href="posts_delete.php">削除</a>';
-	}
-?>
 <th>Comment</th>
 <th>Datetime</th>
 </tr>
-<?php foreach ($res as $value) : ?>
+<?php foreach ($res1 as $value) : ?>
 <tr align="center">
 <td><?php echo $value['id']; ?></td>
 <td><?php echo $value['name']; ?></td>
 <td><?php echo $value['title']; ?></td>
 <td><?php echo $value['text']; ?></td>
 <td><?php echo $value['datetime']; ?></td>
+
+<?php
+	if (isset($_SESSION['id']))
+		if ($value['user_id'] === $_SESSION['id']) : ?>
+<td>
+</form>
+<form action="posts_delete.php" method="POST">
+<input type="hidden" name="id" value="<?php echo $value['id']; ?>" />
+<input type="submit" value="Delete" />
+</form>
+</td>
+<?php endif; ?>
 </tr>
 <?php endforeach; ?>
 </table>
 <?php
-	if ($count === 0) {
-		echo '投稿はまだありません';
-	}
+			if ($count === 0) {
+				echo '投稿はまだありません';
+			}
 ?>
 <?php
 } catch (PDOException $e) {
@@ -78,11 +89,24 @@ try {
 	exit;
 }
 ?>
-<form action="#" method="POST">
-<Name<input type="name">
+</div>
+<p class="border"></p>
+<h3 class="contact-title">Contact</h3>
+<div id="contact">
+<form  action="contact.php" method="POST">
+<p class="contact-name">Name</p>
+<p><input type="name" name="name" /></p>
+<p class="contact-mail"><p>Mail</p>
+<input type="mail" name="mail" /></p>
+<p class="contact-comment">Comment</p>
+<textarea type="text" name="text" cols="40" rows="7"></textarea>
+<br><input type="submit" value="submit" />
+</form>
+</div>
 <footer>
 <p>&copy; 2019-Posted bulletin board</p>
 </footer>
 </body>
 </center>
 </html>
+
